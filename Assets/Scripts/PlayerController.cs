9f6a6e183ba6b2b5fe25db56d86d49653b30757c
elsword50716 <c0970305160@gmail.com>
2020-09-19 19:39:18 +0800

    砲台功能完善版

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,19 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float jumpPower = 0.03f;
-
     private float horizontalMove = 0f;
-    private Vector2 _moveAxis;
     private Rigidbody2D rbody2D;
     private Transform originalParent;
     private Transform shooterController_temp;
-    private bool canGoUp = false;
-    private bool onSeat = false;
 
+    private bool onShooterController = false;
+    
+
+    private PlayerInput playerInput;
 
     private void Awake()
     {
+        playerInput = GetComponent<PlayerInput>();
         rbody2D = GetComponent<Rigidbody2D>();
         originalParent = transform.parent;
     }
@@ -26,37 +27,30 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(onSeat)
+        if (onShooterController)
             return;
 
-        if (canGoUp)
+        /*if (canGoUp)
             transform.position += new Vector3(_moveAxis.x, _moveAxis.y, 0f) * walkSpeed * Time.deltaTime;
         else
-            transform.position += new Vector3(_moveAxis.x, 0f, 0f) * walkSpeed * Time.deltaTime;
+            transform.position += new Vector3(_moveAxis.x, 0f, 0f) * walkSpeed * Time.deltaTime;*/
 
+        if (horizontalMove > 0)
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        if (horizontalMove < 0)
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+
+        rbody2D.velocity = new Vector2(horizontalMove * walkSpeed, rbody2D.velocity.y);
+        
+        
     }
 
 
 
     public void Move(InputAction.CallbackContext context)
     {
-        if(onSeat)
-            return;
-
-        horizontalMove = Mathf.Abs(context.ReadValue<Vector2>().x);
-        _moveAxis = context.ReadValue<Vector2>();
-        Debug.Log(_moveAxis);
-
-        if (onSeat)
-            return;
-
-        if (context.ReadValue<Vector2>().x > 0)
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        if (context.ReadValue<Vector2>().x < 0)
-            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-
-
-
+        horizontalMove = context.ReadValue<Vector2>().x;
+        Debug.Log(horizontalMove);
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -64,9 +58,11 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             rbody2D.AddForce(new Vector2(0f, jumpPower));
-            Debug.Log("ship add force");
+            if (transform.parent.CompareTag("ShooterController"))
+            {
+                ExitShooterController();
+            }
         }
-
 
 
     }
@@ -80,49 +76,46 @@ public class PlayerController : MonoBehaviour
         if (!context.performed)
             return;
         Debug.Log("Back");
-        if (transform.parent.CompareTag("Seats"))
+        if (transform.parent.CompareTag("ShooterController"))
         {
-            ExitSeat();
+            ExitShooterController();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.parent.CompareTag("Stairs"))
-        {
-            rbody2D.gravityScale = 0;
-            canGoUp = true;
-        }
 
-        if (other.transform.CompareTag("Seats"))
+
+        if (other.transform.CompareTag("ShooterController"))
         {
-            shooterController_temp = other.transform.parent;
-            shooterController_temp.GetComponent<ShooterController>().enabled = true;
-            shooterController_temp.GetComponent<PlayerInput>().enabled = true;
-            onSeat = true;
-            transform.parent = other.transform;
-            transform.localPosition = Vector3.zero;
-            transform.localEulerAngles = Vector3.zero;
+            EnterShooterController(other.transform);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.transform.parent.CompareTag("Stairs"))
-        {
-            rbody2D.gravityScale = 1;
-            canGoUp = false;
-        }
+
     }
 
-    private void ExitSeat()
+    private void ExitShooterController()
     {
-        onSeat = false;
+        onShooterController = false;
         transform.parent = originalParent;
-        _moveAxis = Vector2.zero;
+        shooterController_temp.GetComponent<PlayerInput>().enabled = false;
         shooterController_temp.GetComponent<ShooterController>().enabled = false;
         shooterController_temp.GetComponent<ShooterController>().isShootting = false;
-        shooterController_temp.GetComponent<PlayerInput>().enabled = false;
+    }
+
+    private void EnterShooterController(Transform controller)
+    {
+        shooterController_temp = controller.transform;
+        shooterController_temp.GetComponent<ShooterController>().enabled = true;
+        shooterController_temp.GetComponent<PlayerInput>().enabled = true;
+        onShooterController = true;
+        transform.parent = shooterController_temp;
+        rbody2D.velocity = Vector2.zero;
+        transform.localPosition = Vector3.zero;
+        transform.localEulerAngles = Vector3.zero;
     }
 
 }
--- Assets/Scripts/ShooterController.cs
+++ Assets/Scripts/ShooterController.cs
@@ -12,9 +12,11 @@ public class ShooterController : MonoBehaviour
     public GameObject bulletPrefab;
     public float BulletSpeed = 1f;
     public float FireRate = 0.2f;
+    public float[] shooterAngleRange = new float[2];
 
     public float gunMovingDegree = 1f;
     private float timer = 0f;
+    private float gunRotationZ = 0f;
     private int counter = 0;
     private Vector2 moveInput = Vector2.zero;
     private Transform[] firePoint;
@@ -24,7 +26,7 @@ public class ShooterController : MonoBehaviour
     {
         var childCount = gunPivotPoint.childCount;
         firePoint = new Transform[childCount];
-        
+
         for (int i = 0; i < firePoint.Length; i++)
         {
             firePoint[i] = gunPivotPoint.GetChild(i).GetChild(0);
@@ -36,37 +38,41 @@ public class ShooterController : MonoBehaviour
     void Update()
     {
 
-        if (Mathf.Abs(gunPivotPoint.rotation.z) < 90f)
-        {
-            if (isGunHorizontal)
-            {
-                gunPivotPoint.Rotate(new Vector3(0f, 0f, -moveInput.x * gunMovingDegree * Time.deltaTime));
-            }
-            else
-            {
-                gunPivotPoint.Rotate(new Vector3(0f, 0f, -moveInput.y * gunMovingDegree * Time.deltaTime));
-            }
+        gunRotationZ = ClampAngle(gunPivotPoint.localEulerAngles.z, -60, 60);
 
+        gunPivotPoint.localEulerAngles = new Vector3(0f, 0f, gunRotationZ);
+
+        if (isGunHorizontal)
+        {
+            gunPivotPoint.Rotate(new Vector3(0f, 0f, -moveInput.x * gunMovingDegree * Time.deltaTime));
         }
+        else
+        {
+            gunPivotPoint.Rotate(new Vector3(0f, 0f, -moveInput.y * gunMovingDegree * Time.deltaTime));
+        }
+
+
+
 
         if (isShootting)
         {
             timer += Time.deltaTime;
 
-            if(timer > FireRate){
+            if (timer > FireRate)
+            {
                 counter++;
                 timer = 0;
 
-                if(counter == firePoint.Length)
-                counter = 0;
+                if (counter == firePoint.Length)
+                    counter = 0;
 
                 FireBullet(counter % firePoint.Length);
             }
 
-            
 
 
-            
+
+
         }
     }
 
@@ -89,5 +95,14 @@ public class ShooterController : MonoBehaviour
         var bulletRbody2D = bullet.GetComponent<Rigidbody2D>();
         bulletRbody2D.velocity = firePoint[index].up * BulletSpeed;
         bullet.transform.up = firePoint[index].up;
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+        return Mathf.Clamp(angle, min, max);
     }
 }
