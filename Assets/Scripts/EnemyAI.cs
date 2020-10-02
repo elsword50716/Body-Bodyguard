34f9294ed58764afea6703a26d7b67f51b994e14
Msi Laptop\elsword <c0970305160@gmail.com>
2020-10-02 12:37:08 +0800

    EnemyAI新增FindPath
    會友卡牆角bug

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -13,6 +13,9 @@ public class EnemyAI : MonoBehaviour
     public float NextRoamingPositionDelay = 1f;
     public LayerMask obstaclesLayer;
     public Transform BulletPrefab;
+    [Range(0, 180)] public float findPathScanDegreeRange = 90f;
+    [Range(1, 10)] public float colliderRadiusMulti = 2f;
+    public float scanPerDegree = 5f;
 
     [Header("敵人資料")]
     public EnemyData enemyData = new EnemyData();
@@ -27,7 +30,7 @@ public class EnemyAI : MonoBehaviour
     }
     private Vector3 startingPosotion;
     private Vector3 roamPosotion;
-    private Vector3 targetPosotion;
+    private Vector3 targetPosition;
     private bool isReachTarget = false;
     private State state;
     private float nextShootTimer = 0f;
@@ -35,6 +38,7 @@ public class EnemyAI : MonoBehaviour
     private Transform bulletPool;
     private Animator animator;
     private CircleCollider2D circleCollider2D;
+    private Rigidbody2D Rbody2D;
 
 
     private void Awake()
@@ -44,6 +48,7 @@ public class EnemyAI : MonoBehaviour
             bulletPool = GetComponentInChildren<BulletManager>().transform;
         animator = GetComponentInChildren<Animator>();
         circleCollider2D = GetComponent<CircleCollider2D>();
+        Rbody2D = GetComponent<Rigidbody2D>();
         state = State.Roaming;
     }
 
@@ -73,42 +78,12 @@ public class EnemyAI : MonoBehaviour
                 break;
 
             case State.ChaseTarget:
-                if (Vector3.Distance(transform.position, Ship.position) > enemyData.detectShipRange)
-                    state = State.Roaming;
+                //if (Vector3.Distance(transform.position, Ship.position) > enemyData.detectShipRange)
+                //    state = State.Roaming;
 
-                if (Vector3.Distance(transform.position, Ship.position) < enemyData.ClosestDistanceToShip - avoidShaking)
-                {
+                FindThePath();
 
-                    //move back
-                    MoveTo(Ship.position, false, false);
-                }
-                else if (Vector3.Distance(transform.position, Ship.position) > enemyData.ClosestDistanceToShip + avoidShaking)
-                {
-                    //move toward
-                    MoveTo(Ship.position, true, false);
-                }
-                else
-                {
-                    //attcak
-                    transform.RotateAround(Ship.position, Vector3.forward, enemyData.inAttackRangeMoveSpeed * Time.deltaTime);
 
-                    transform.rotation = Quaternion.identity;
-
-                    if (isVirus)
-                    {
-                        var spriteUpDir = (Ship.position - transform.position).normalized;
-                        GetComponentInChildren<SpriteRenderer>().transform.up = spriteUpDir;
-                    }
-
-                    if (nextShootTimer > enemyData.FireRate)
-                    {
-                        //Attack();
-                        state = State.Attacking;
-                        nextShootTimer = 0;
-                    }
-                    else
-                        nextShootTimer += Time.deltaTime;
-                }
 
                 Debug.DrawLine(transform.position, Ship.position, Color.red);
 
@@ -146,10 +121,14 @@ public class EnemyAI : MonoBehaviour
                 var spriteUpDir = (targetPosition - transform.position).normalized;
                 GetComponentInChildren<SpriteRenderer>().transform.up = spriteUpDir;
             }
-            transform.Translate((targetPosition - transform.position).normalized * speedMultiply * Time.deltaTime);
+            Rbody2D.velocity = (targetPosition - transform.position).normalized * speedMultiply;
+            //transform.Translate((targetPosition - transform.position).normalized * speedMultiply * Time.deltaTime);
         }
         else
-            transform.Translate((transform.position - targetPosition).normalized * speedMultiply * Time.deltaTime);
+        {
+            Rbody2D.velocity = (transform.position - targetPosition).normalized * speedMultiply;
+            //transform.Translate((transform.position - targetPosition).normalized * speedMultiply * Time.deltaTime);
+        }
     }
 
     private void FindTarget()
@@ -174,12 +153,12 @@ public class EnemyAI : MonoBehaviour
 
         if (hit2D.collider != null)
         {
-            //FindThePath(hit2D);
             roamPosotion = new Vector3(hit2D.point.x, hit2D.point.y) - dir.normalized * circleCollider2D.radius * 1.5f;
 
         }
         if (dir.magnitude <= 0.5f)
         {
+            Rbody2D.velocity = Vector2.zero;
             if (nextMoveTimer > NextRoamingPositionDelay)
                 roamPosotion = GetRoamingPostion();
             else
@@ -224,9 +203,135 @@ public class EnemyAI : MonoBehaviour
 
     }
 
-    private void FindThePath(RaycastHit2D hit2D)
+    private void FindThePath()
     {
-        Debug.Log("FindingPath");
+
+
+        var toShipDir = Ship.position - transform.position;
+
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, toShipDir.normalized, toShipDir.magnitude, obstaclesLayer);
+
+        if (hit2D.collider == null)
+        {
+            targetPosition = Ship.position;
+
+            if (Vector3.Distance(transform.position, targetPosition) < enemyData.ClosestDistanceToShip - avoidShaking)
+            {
+                //move back
+                MoveTo(targetPosition, false, false);
+            }
+            else if (Vector3.Distance(transform.position, targetPosition) > enemyData.ClosestDistanceToShip + avoidShaking)
+            {
+                //move toward
+                MoveTo(targetPosition, true, false);
+            }
+            else
+            {
+                //attcak
+                Rbody2D.velocity = Vector2.zero;
+
+                //transform.RotateAround(Ship.position, Vector3.forward, enemyData.inAttackRangeMoveSpeed * Time.deltaTime);
+
+                //transform.rotation = Quaternion.identity;
+
+                if (isVirus)
+                {
+                    var spriteUpDir = (Ship.position - transform.position).normalized;
+                    GetComponentInChildren<SpriteRenderer>().transform.up = spriteUpDir;
+                }
+
+                if (nextShootTimer > enemyData.FireRate)
+                {
+                    //Attack();
+                    state = State.Attacking;
+                    nextShootTimer = 0;
+                }
+                else
+                    nextShootTimer += Time.deltaTime;
+            }
+
+            return;
+        }
+
+        Rbody2D.velocity = Vector2.zero;
+
+        var firstCollider_temp = hit2D.collider;
+
+        var contactPoint_N = Vector2.zero;
+        var contactPoint_P = Vector2.zero;
+
+        var nextNodePosition = Vector2.zero;
+
+        for (float i = 0; i < findPathScanDegreeRange; i += scanPerDegree)
+        {
+            var RotatedX_N = (toShipDir.x * Mathf.Cos(-i * Mathf.Deg2Rad) - toShipDir.y * Mathf.Sin(-i * Mathf.Deg2Rad));
+            var RotatedY_N = (toShipDir.x * Mathf.Sin(-i * Mathf.Deg2Rad) + toShipDir.y * Mathf.Cos(-i * Mathf.Deg2Rad));
+            var RotatedX_P = toShipDir.x * Mathf.Cos(i * Mathf.Deg2Rad) - toShipDir.y * Mathf.Sin(i * Mathf.Deg2Rad);
+            var RotatedY_P = toShipDir.x * Mathf.Sin(i * Mathf.Deg2Rad) + toShipDir.y * Mathf.Cos(i * Mathf.Deg2Rad);
+
+            Vector3 Rotate_N = new Vector2(RotatedX_N, RotatedY_N);
+            Vector3 Rotate_P = new Vector2(RotatedX_P, RotatedY_P);
+
+            Debug.DrawLine(transform.position, transform.position + Rotate_N, Color.blue);
+            Debug.DrawLine(transform.position, transform.position + Rotate_P, Color.yellow);
+
+            hit2D = Physics2D.Raycast(transform.position, Rotate_N.normalized, toShipDir.magnitude, obstaclesLayer);
+            RaycastHit2D hit2D1 = Physics2D.Raycast(transform.position, Rotate_P.normalized, toShipDir.magnitude, obstaclesLayer);
+
+            if (hit2D.collider != null && hit2D1.collider != null)
+            {
+                contactPoint_N = hit2D.point;
+                contactPoint_P = hit2D1.point;
+            }
+            else if (hit2D.collider == null && hit2D1.collider == null)
+            {
+                var distance_N = ProjectPointConverter(transform.position, transform.position + Rotate_N, contactPoint_N) - (Vector2)transform.position;
+                var distance_P = ProjectPointConverter(transform.position, transform.position + Rotate_P, contactPoint_P) - (Vector2)transform.position;
+
+                nextNodePosition = distance_N.magnitude > distance_P.magnitude ? distance_P + (Vector2)transform.position : distance_N + (Vector2)transform.position;
+                Debug.DrawLine(transform.position, nextNodePosition, Color.grey);
+                MoveTo(nextNodePosition, true, false);
+                break;
+            }
+            else
+            {
+                if (hit2D.collider == null)
+                {
+                    nextNodePosition = ProjectPointConverter(transform.position, transform.position + Rotate_N, contactPoint_N);
+                    Debug.DrawLine(transform.position, nextNodePosition, Color.grey);
+                    MoveTo(nextNodePosition, true, false);
+                    break;
+                }
+
+                if (hit2D1.collider == null)
+                {
+                    nextNodePosition = ProjectPointConverter(transform.position, transform.position + Rotate_P, contactPoint_P);
+                    Debug.DrawLine(transform.position, nextNodePosition, Color.grey);
+                    MoveTo(nextNodePosition, true, false);
+                    break;
+                }
+
+            }
+
+        }
+
+
+
+    }
+
+    //找出投影點並轉換成新向量
+    private Vector2 ProjectPointConverter(Vector2 startPoint, Vector2 endPoint, Vector2 anyPoint)
+    {
+        float a = endPoint.y - startPoint.y;
+        float b = startPoint.x - endPoint.x;
+        float c = endPoint.x * startPoint.y - startPoint.x * endPoint.y;
+        float denominator = (a * a + b * b);
+        float t = (a * anyPoint.x + b * anyPoint.y + c) / denominator;
+
+        var theProjectedPoint = new Vector2(anyPoint.x - a * t, anyPoint.y - b * t);
+
+        Debug.DrawLine(anyPoint, anyPoint + (theProjectedPoint - anyPoint).normalized * circleCollider2D.radius * colliderRadiusMulti);
+        return anyPoint + (theProjectedPoint - anyPoint).normalized * circleCollider2D.radius * colliderRadiusMulti;
     }
 
     public void GetDamaged(float damage)
